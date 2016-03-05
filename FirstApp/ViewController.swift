4b5deab96973accea49e3cca8207d7e9e6ab7a4f
Ottama <nnm_unname@hotmail.com>
2016-03-05 20:45:46 +0700

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by tpidech on 3/3/2559 BE.
+//  Copyright © 2559 tpidech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func example() {
+        let str = ""
+        let otherStr = ""
+        print("\(str) and \(otherStr)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
